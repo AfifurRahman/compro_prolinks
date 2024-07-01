d0a4edd9d934debb8639694535b1def3a84a253d
Jean Vieri <jeansusilo99@gmail.com>
2024-07-01 16:08:47 +0700

    tes

--- resources/views/frontend/home/company.blade.php
+++ resources/views/frontend/home/company.blade.php
@@ -1,15 +1,15 @@
 @extends('layouts.app_frontend')
 
 @section('content')
-    <link href="{{ url('frontend/css/insight.css') }}" rel="stylesheet" type="text/css" /> 
+    <link href="{{ url('frontend/css/company.css') }}" rel="stylesheet" type="text/css" /> 
 
     <div class="company-container-1">
         <div class="container">
-            <div class="about-company-1">
+            <div class="row">
                 <p class="about-company-title">Prolinks Virtual Data Room - more than just technology</p>
                 <p class="about-company-text">At Prolinks, we are dedicated to revolutionizing the way businesses handle their most sensitive and critical transactions. As a leading provider of Virtual Data Room (VDR) applications, we offer state-of-the-art solutions designed to streamline, secure, and simplify complex processes such as mergers and acquisitions, financial transactions, and due diligence. </p>
             </div>
-            <div class="about-company-2">
+            <div class="row">
                 <p class="about-company-mission">Our mission</p>
                 <p class="about-company-text">Our mission is to empower organizations with the tools they need to manage their data efficiently and securely. We understand the importance of data integrity and confidentiality, and we strive to deliver unparalleled VDR services that exceed our clients' expectations.</p>
             </div>
@@ -18,23 +18,35 @@
 
     <div class="company-container-2">
         <div class="container">
-            <div class="row">
+            <div class="awards-row">
                 <div class="award-row">
-                    <p>#1</p>
-                    <p>Indonesian VDR</p>
+                    <p class="award-row-up">#1</p>
+                    <p class="award-row-bottom">Indonesian VDR</p>
                 </div>
                 <div class="award-row">
-                    <p>10+</p>
-                    <p>Trusting clients</p>
+                    <p class="award-row-up">10+</p>
+                    <p class="award-row-bottom">Trusting clients</p>
                 </div>
                 <div class="award-row">
-                    <p>13</p>
-                    <p>Completed deals</p>
+                    <p class="award-row-up">13</p>
+                    <p class="award-row-bottom">Completed deals</p>
                 </div>
                 <div class="award-row">
-                    <p>IDR 1.5B</p>
-                    <p>Completed transactions</p>
+                    <p class="award-row-up">IDR 1.5B</p>
+                    <p class="award-row-bottom">Completed transactions</p>
                 </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="company-container-3">
+        <div class="container">
+            <div class="company-profile">
+                <div class="about-synergy">
+                    <p class="company-name">Synergy Asia</p>
+                    <p class="company-mission">Our mission is to empower organizations with the tools they need to manage their data efficiently and securely. We understand the importance of data integrity and confidentiality, and we strive to deliver unparalleled VDR services that exceed our clients' expectations.</p>
+                </div> 
+                <img src="{{ url('frontend/images/synergy.png') }}" class="synergy-img"/>
             </div>
         </div>
     </div>
